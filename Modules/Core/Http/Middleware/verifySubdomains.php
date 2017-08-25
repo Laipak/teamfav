08a0ca06b930dbff2c3e29b91a32958b8716a9d6
Park <park8lai@gmail.com>
2017-08-25 17:54:15 +0800

    Add check subdomain middleware

--- Modules/Core/Http/Middleware/verifySubdomains.php
+++ Modules/Core/Http/Middleware/verifySubdomains.php
@@ -2,7 +2,7 @@
 
 namespace Modules\Core\Http\Middleware;
 
-use Session, Request, Closure;
+use Exception, Session, Request, Closure;
 use Illuminate\Support\Facades\Auth;
 use Modules\Team\Entities\Team;
 
@@ -13,10 +13,9 @@ class verifySubdomains
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
         // Get all segments of the domain
         $domains = explode('.', Request::server('HTTP_HOST'));
@@ -25,12 +24,13 @@ class verifySubdomains
         // meaning it should look: teamname.domain.tld 
         if (count($domains) === 3 && $domains[0] !== 'www') {
             
-            $team = Team::where('nickname', $domains[0])->first();
-
-            // No team with that nickname is found
-            // Abort and redirect to 404 page
-            if (!$team) abort(404, $domains[0] . ' is not a registered team. Create your team now!');
-            // ------
+            // Redirect to 404 page if the nickname has not been registered
+            try {
+                $team = Team::where('nickname', $domains[0])->firstOrFail();
+                // --
+            } catch (Exception $e) {
+                abort(404, $domains[0] . ' is not a registered team. Create your team now!');
+            }
             
             // Put into session if the the team is found
             Session::put('current_team', $team->nickname);
